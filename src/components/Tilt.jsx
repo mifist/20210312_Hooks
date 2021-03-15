7df2f22e25077737e7d18b0e3036a7c53cfc6b0f
romannikiforov <escaro@ukr.net>
2021-03-15 20:24:15 +0200

    timer

--- src/components/Tilt.jsx
+++ src/components/Tilt.jsx
@@ -1,19 +1,25 @@
 import {useRef, useEffect} from "react"
-// import VanillaTilt from "vanilla-tilt";
+import VanillaTilt from "vanilla-tilt";
 
 //https://micku7zu.github.io/vanilla-tilt.js/
 
-/* 🎁 Vanilla tilt params 
-  glare: true,
-  maxGlare: 0.5,
-  speed: 400,
-  startX: -20,
-  startY: -20,
-  max: 25
-*/
-
 const Tilt = props => {
-  return <div className="custom-tilt" />
+  const tiltRef = useRef();
+
+  useEffect(() => {
+       const tr = tiltRef.current;
+       VanillaTilt.init(tr, {
+        glare: true,
+        maxGlare: 0.5,
+        speed: 400,
+        startX: -20,
+        startY: -20,
+        max: 25
+       });
+       return () => tr.vanilaTilt.destroy();
+  }, [])
+
+  return <div  ref={tiltRef} className="custom-tilt" />
 }
 
 export default Tilt
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,13 +1,41 @@
-import {useState} from "react"
+import {useState, useRef} from "react"
+import { useEffect } from "react/cjs/react.development"
+
+// lapse, running, clear
 
 const Timer = () => {
+  const [lapse, setLapse] = useState(0)
+  const [running, setRunning] = useState(false)
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    return () => clearInterval(intervalRef.current)
+  }, [])
+
+
+  const handleRunning = () => {
+    if(running){
+      clearInterval(intervalRef.current)
+    } else {
+      const startTime = Date.now() - lapse
+      intervalRef.current = setInterval(() => setLapse(Date.now() - startTime), 0)
+    }
+    setRunning(!running)
+  }
+
+  const handleClear = () => {
+    clearInterval(intervalRef.current)
+    setRunning(false)
+    setLapse(0)
+  }
+
   return (
     <>
-      <h3>Time: </h3>
-      <button onClick={() => {}} className="btn btn-primary btn-lg mr-3">
-        Start
+      <h3>Time: {lapse}</h3>
+      <button onClick={handleRunning} className="btn btn-primary btn-lg mr-3">
+        {running ? "Stop" : "Start"}
       </button>
-      <button onClick={() => {}} className="btn btn-success btn-lg">
+      <button onClick={handleClear} className="btn btn-success btn-lg">
         Clear
       </button>
     </>
